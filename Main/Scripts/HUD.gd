da885b5aff34df8fb474bbc596ca46e50e99609e
robertarnborg <robert.arnborg@gmail.com>
2019-08-29 18:54:43 +0200

    FixedHud

--- Main/Scripts/HUD.gd
+++ Main/Scripts/HUD.gd
@@ -4,6 +4,10 @@ func _init():
 	Global.HUD = self
 
 
+func _ready():
+	$PlayAgain.connect("button_up",get_parent(),"_restart_level")
+
+
 func update_hud():
 	$Control/P1Health.value = Global.player_1.health
 	$Control/P2Health.value = Global.player_2.health
@@ -14,3 +18,4 @@ func win(player_1_win):
 		$P1Win.show()
 	else:
 		$P2Win.show()
+	$PlayAgain.show()
--- Main/Scripts/Level.gd
+++ Main/Scripts/Level.gd
@@ -2,3 +2,15 @@ extends Node2D
 
 func _init():
 	Global.Level_Node = self
+
+
+func _on_Player1Ship_death():
+	Global.HUD.win(true)
+
+
+func _on_Player2Ship_death():
+	Global.HUD.win(false)
+
+
+func _restart_level():
+	get_tree().reload_current_scene()
