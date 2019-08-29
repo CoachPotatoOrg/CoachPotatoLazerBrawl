e4f62d68007fb11c9d384b72e110cf971fad4429
robertarnborg <robert.arnborg@gmail.com>
2019-08-29 15:38:02 +0200

    GamepadControllers

---
+++ Main/Scripts/Player/CannonBall.gd
@@ -0,0 +1,57 @@
+extends KinematicBody2D
+
+export var bullet_speed = 100
+export var damage = 10
+
+var bounces = 0
+export var bounce_limit = 2
+var ricochet = false
+
+var motion = Vector2()
+
+var collision_info
+var collider
+var collision_point
+
+export (PackedScene) var hit_explosion
+
+
+func check_bounces():
+	if ricochet:
+		if bounces >= bounce_limit:
+			queue_free()
+	else:
+		queue_free()
+
+
+func _physics_process(delta):
+	collision_info = move_and_collide(motion * delta)
+	if collision_info:
+		ricochet = false
+		randomize()
+		var dice_roll = randi() % 8
+		if dice_roll >= 7:
+			motion = motion.bounce(collision_info.normal)
+			look_at(motion)
+			bounces += 1
+			add_to_group("enemy")
+			ricochet = true
+		collider = collision_info.collider
+		collision_point = collision_info.normal
+		create_explosion(collision_point)
+		if collider != null:
+			if collider.is_in_group("enemy"):
+				print("boom")
+				collider.take_damage(damage)
+				print (collider.health)
+
+
+func create_explosion(collision_point):
+	var hit_explosion_instance = hit_explosion.instance()
+	
+	hit_explosion_instance.rotation_degrees = rad2deg(atan2(collision_point.y, collision_point.x))# * 360/2/PI #THE ROTATION Calculation BASED UPON THE COLLISIONPOINT NORMAL
+	hit_explosion_instance.global_position = global_position
+	
+	Global.Level_Node.add_child(hit_explosion_instance)
+	hit_explosion_instance.check_impact_type(collider)
+	check_bounces()
--- Main/Scripts/Player/PlayerShip.gd
+++ Main/Scripts/Player/PlayerShip.gd
@@ -2,13 +2,23 @@ extends KinematicBody2D
 
 export var player_id = 1
 
+export var ship_health = 100
+
 export (int) var speed = 200
+export (float) var acceleration = 0.5
 export (float) var rotation_speed = 1.5
 
 export (PackedScene) var standard_cannon_laser
 
 var velocity = Vector2()
 var rotation_dir = 0
+
+var friction = 0.2
+
+onready var broadside_left = $FireBroadsideLeft
+onready var broadside_right = $FireBroadsideRight
+
+var can_fire = true
 
 func get_input():
     rotation_dir = 0
@@ -22,11 +32,12 @@ func get_input():
     if Input.is_action_pressed("upP%s" % player_id):
         velocity = Vector2(speed, 0).rotated(rotation)
 
+
 func _physics_process(delta):
     get_input()
     rotation += rotation_dir * rotation_speed * delta
     velocity = move_and_slide(velocity)
 
 
-func fire_cannon():
+func fire_cannon_left_side():
 	pass
